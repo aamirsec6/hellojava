59b96e7e1f30812ad792ecfcc69570f25f52f868
aamirsec6 <aamirsec6@gmail.com>
2021-08-28 17:07:38 +0530

    from faizan

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Simple{
     public static void main(String args[]){  
         System.out.println("Hello Java"); 
          System.out.println("Hello Aamir");  
+         System.out.println("Hello Faizan");  
      //test
     }  
 }  
